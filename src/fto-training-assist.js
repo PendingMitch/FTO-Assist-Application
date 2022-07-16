1fd800c447545d1bf304258fcc1c413aefd75960
PendingMitch <70475488+PendingMitch@users.noreply.github.com>
2022-07-16 19:25:01 +0100

    final page

--- src/fto-training-assist.js
+++ src/fto-training-assist.js
@@ -64,8 +64,51 @@ continue_assist = (page) => {
   } else if (page == 3) {
     hidepage(page, page+1)
     createLinkButton("Uniform Guide", "https://drive.google.com/file/d/1LCVDCJ_HDsTkcFAkY8TI-VsvDH9ixH37/view", page)
+  } else if (page == 4) {
+    hidepage(page, page+1)
+  } else if (page == 5) {
+    hidepage(page, "final")
   }
 
 
   scrollTo(0,0)
+}
+
+
+function copyReport() {
+
+  if (document.getElementById("concern_final").value == "") {
+    document.getElementById("concern_final").value = "None (N/A)"
+  }
+
+  let text = "Callsign: " + document.getElementById('Callsign_final').value + '\nDiscord: ' + document.getElementById("Discord_final").value + '\n'
+
+  concern_0 = document.getElementById("concerns_0")
+  text = text + concern_0.getAttribute("head") + concern_0.value + '\n';
+  
+  concern_1 = document.getElementById("concerns_1")
+  text = text + concern_1.getAttribute("head") + concern_1.value + '\n';
+  
+  concern_2 = document.getElementById("concerns_2")
+  text = text + concern_2.getAttribute("head") + concern_2.value + '\n';
+  
+  concern_3 = document.getElementById("concerns_3")
+  text = text + concern_3.getAttribute("head") + concern_3.value + '\n';
+  
+  concern_4 = document.getElementById("concerns_4")
+  text = text + concern_4.getAttribute("head") + concern_4.value + '\n';
+  
+  concern_5 = document.getElementById("concerns_5")
+  text = text + concern_5.getAttribute("head") + concern_5.value + '\n';
+
+  concern_final = document.getElementById("concern_final")
+  text = text + concern_final.getAttribute("head") + concern_final.value + '\n';
+  
+  /* Copy the text inside the text field */
+  navigator.clipboard.writeText(text);
+  
+  document.getElementById('report').innerHTML = "<b><u>COPIED TO CLIPBOARD</u></b> <br><br>" + text.replaceAll('\n', "<br>");
+
+  
+  
 }
