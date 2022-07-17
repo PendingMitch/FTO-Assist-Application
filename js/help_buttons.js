c056af0c8eb37045052c3e2f085716740b84946c
PendingMitch <70475488+PendingMitch@users.noreply.github.com>
2022-07-18 00:56:59 +0100

    Styling Changes

--- js/help_buttons.js
+++ js/help_buttons.js
@@ -47,16 +47,19 @@ createLinkButton = (button_name, button_link, color) => {
   button.innerHTML = button_name;
   a_tag.appendChild(button);
 
+  copy_icon_div = document.createElement("div")
   copy_icon = document.createElement("img");
   copy_icon.src = "static/images/copy_icon.svg";
   copy_icon.classList.add("copy_icon");
   copy_icon.addEventListener("click", () => {
     navigator.clipboard.writeText(a_tag.href);
   });
+  copy_icon_div.classList.add("copy_icon_div")
+  copy_icon_div.appendChild(copy_icon)
 
   div = document.createElement("div");
   div.appendChild(a_tag);
-  div.appendChild(copy_icon);
+  div.appendChild(copy_icon_div);
   document.getElementById("helpful_links").appendChild(div);
 };
 
